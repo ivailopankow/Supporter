b9be38317038cafb86a055d4a246777d1bb7ca47
Ivaylo Pankov <ivaylo.pankow@gmail.com>
2016-10-30 16:30:58 +0200

    Add resources, dependencies first controller
    Initial support for project start.

---
+++ src/main/java/supporter/Controllers/HomeController.java
@@ -0,0 +1,17 @@
+package supporter.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Ivaylo on 30-Oct-16.
+ */
+@Controller
+public class HomeController {
+    private static final String HOME_PAGE = "index";
+
+    @RequestMapping("/")
+    public String homePage(){
+        return HOME_PAGE;
+    }
+}
---
+++ src/main/java/supporter/SupporterEntryPoint.java
@@ -0,0 +1,14 @@
+package supporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Ivaylo on 30-Oct-16.
+ */
+@SpringBootApplication
+public class SupporterEntryPoint {
+    public static void main(String[] args) {
+        SpringApplication.run(SupporterEntryPoint.class, args);
+    }
+}
