7b1a30eaae197593936f66253b66e5ab1920edd4
Ivaylo Pankov <ivaylo.pankow@gmail.com>
2016-11-12 11:36:00 +0200

    View ticket by id

---
+++ src/main/java/supporter/controllers/TicketsController.java
@@ -0,0 +1,36 @@
+package supporter.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import supporter.models.Ticket;
+import supporter.services.ticket.TicketService;
+
+/**
+ * Created by Ivaylo on 12-Nov-16.
+ */
+
+@Controller
+public class TicketsController {
+
+    private static final String TICKET = "ticket";
+    private static final String TICKET_VIEW = "tickets/view";
+    private final static String LATEST_FIVE_TICKETS = "latestFiveTickets";
+
+    @Autowired
+    private TicketService ticketService;
+
+    @RequestMapping("/tickets/view/{id}")
+    public String viewTicket(@PathVariable("id") Long ticketId, Model model){
+        Ticket ticket = ticketService.findById(ticketId);
+        List<Ticket> asideTickets = ticketService.findLatest5();
+        model.addAttribute(TICKET, ticket);
+        model.addAttribute(LATEST_FIVE_TICKETS, asideTickets);
+        return TICKET_VIEW;
+    }
+}
