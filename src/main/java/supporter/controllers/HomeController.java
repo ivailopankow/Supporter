66565d13c3ec76a005a6c1fd2d9f3584840129e5
Ivaylo Pankov <ivaylo.pankow@gmail.com>
2016-11-12 10:54:20 +0200

    Home view initial implementation.
    It shows three mocked tickets.

--- src/main/java/supporter/controllers/HomeController.java
+++ src/main/java/supporter/controllers/HomeController.java
@@ -1,7 +1,14 @@
 package supporter.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import supporter.models.Ticket;
+import supporter.services.ticket.TicketService;
 
 /**
  * Created by Ivaylo on 30-Oct-16.
@@ -9,9 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     private static final String HOME_PAGE = "index";
+    private static final String LATEST_FIVE_TICKETS = "latestFiveTickets";
+    private static final String LATEST_THREE_TICKETS = "latestThreeTickets";
+
+    @Autowired
+    private TicketService ticketService;
 
     @RequestMapping("/")
-    public String homePage(){
+    public String homePage(Model model){
+        List<Ticket> tickets = ticketService.findLatest5();
+        model.addAttribute(LATEST_FIVE_TICKETS, tickets);
+        List<Ticket> latestThreeTickets = tickets.subList(0, 3);
+        model.addAttribute(LATEST_THREE_TICKETS, latestThreeTickets);
         return HOME_PAGE;
     }
 }
