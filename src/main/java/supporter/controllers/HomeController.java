639b761e9ee29df218badb67331a08fab75caeda
Ivaylo Pankov <ivaylo.pankow@gmail.com>
2016-11-30 23:21:29 +0200

    Update home page.

--- src/main/java/supporter/controllers/HomeController.java
+++ src/main/java/supporter/controllers/HomeController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import supporter.models.Category;
 import supporter.models.Product;
+import supporter.services.category.CategoryService;
 import supporter.services.product.ProductService;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Ivaylo on 30-Oct-16.
@@ -17,13 +21,26 @@ import java.util.List;
 public class HomeController {
 
     @Autowired
-    private ProductService productService;
+    private CategoryService categoryService;
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Product> allProducts = productService.findAll();
-        model.addAttribute("products", allProducts);
+        List<Category> categories = categoryService.findAll(false);
+        model.addAttribute("categories", categories);
         return "home/index";
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public String showCategoryProducts(Model model, @PathVariable Integer categoryId) {
+        if (!this.categoryService.exists(categoryId)) {
+            return "redirect:/";
+        }
+
+        Category category = this.categoryService.findById(categoryId);
+        Set<Product> products = category.getProducts();
+        model.addAttribute("category", category);
+        model.addAttribute("products", products);
+        return "home/category-products-list";
     }
 
     @RequestMapping("/error/403")
